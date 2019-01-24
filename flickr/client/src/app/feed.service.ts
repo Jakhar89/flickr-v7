8071dcab6ddc1bd043e0333fc7098afd0f690f6a
Jack <jakharemail@yahoo.com>
2019-01-24 20:10:37 +1100

    Node Express Angular 7 on Heroku

--- flickr/client/src/app/feed.service.ts
+++ flickr/client/src/app/feed.service.ts
@@ -13,9 +13,9 @@ export class FeedService {
     let url;
     // included localhost:3000 for running/testing sole Angular 6 app
     if (key) {
-      url = `http://localhost:3000/app/tasks/flickr/${key}`;
+      url = `/app/tasks/flickr/${key}`;
     }else {
-      url = `http://localhost:3000/app/tasks/flickr`;
+      url = `/app/tasks/flickr`;
     }
     return this.http.get(url)
    // Call map on the response observable to get the parsed object
@@ -26,9 +26,9 @@ export class FeedService {
       let url;
       // included localhost:3000 for running/testing sole Angular 6 app
       if (key) {
-        url = `http://localhost:3000/app/tasks/dogs?key=${key}`;
+        url = `/app/tasks/dogs?key=${key}`;
       }else {
-        url = `http://localhost:3000/app/tasks/dogs`;
+        url = `/app/tasks/dogs`;
       }
       return this.http.get(url)
      
