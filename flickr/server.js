127d3e129fc9ad30f61ca373abd71b5b4f9fbc54
Jack <jakharemail@yahoo.com>
2019-01-24 20:02:18 +1100

    Angular-7 Node Express deploying on heroku

--- flickr/server.js
+++ flickr/server.js
@@ -5,7 +5,7 @@ var index = require('./routes/index');
 var tasks = require('./routes/tasks');
 var bodyParser = require('body-parser');
 
-var port = 3000;
+var port = process.env.PORT||3000;
 
 var app = express();
 
